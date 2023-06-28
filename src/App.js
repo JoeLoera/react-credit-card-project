888649a9f778fef9bf064b258c751e34bb6eae18
Joe Loera <joeloera7@gmail.com>
2023-06-28 16:57:21 -0700

    Complete Couting React App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { useState } from 'react'
+
 const App = () => {
+  const[count,setCount] = useState(0)
+
+  console.log(count)
   return (
     <div>
+      <button onClick={() => setCount(count +1)}>ADD 1</button>
     </div>
   );
 }
